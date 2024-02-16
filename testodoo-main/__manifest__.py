82f728c916e9cff9571ed7bee2f819ae21e6cab4
itsolutions1 <153736696+itsolutions1@users.noreply.github.com>
2024-02-16 11:10:30 +0530

    testodoo main

---
+++ testodoo-main/__manifest__.py
@@ -0,0 +1,31 @@
+
+# -*- coding: utf-8 -*-
+{
+    'name':'MULTI PRICE',
+    'description':'MULTI PRICE',
+    'author':'Nepal EHR',
+    'website': 'https://www.nepalehr.org/',
+    'application': True,
+    'category': '',
+    'version': '0.1',
+    'depends': [
+        'base',
+        'stock',
+        'product',
+        'purchase',
+        'sale',
+        'account',
+        'board', 
+        "mail", 
+        "procurement",
+        "bahmni_atom_feed",
+        "bahmni_stock" ,
+        "bahmni_account",
+        "stock_account",
+        "maintenance"
+    ],
+    'data': [
+       'views/partnerCusotmizatio'n.xml',
+       'views/product_template.xml,
+    ],
+}
