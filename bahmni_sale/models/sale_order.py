e854558ba7bf7c84538ae6635d86171ca77e64f8
itsolutions1 <153736696+itsolutions1@users.noreply.github.com>
2024-02-19 16:39:46 +0530

    Update sale_order.py

--- bahmni_sale/models/sale_order.py
+++ bahmni_sale/models/sale_order.py
@@ -119,24 +119,23 @@ class SaleOrder(models.Model):
             self.discount = amount_total * self.discount_percentage / 100
 
     @api.onchange('discount', 'discount_percentage', 'discount_type', 'chargeable_amount')
-   def onchange_discount(self):
-    amount_total = self.amount_untaxed + self.amount_tax
-
-    if self.discount_type == 'none':
-        if self.chargeable_amount:
-            self.discount_type = 'fixed'
-            discount = amount_total - self.chargeable_amount
-            self.discount_percentage = (discount / amount_total) * 100
+    def onchange_discount(self):
+        amount_total = self.amount_untaxed + self.amount_tax
+        if self.discount_type == 'none':
+            if self.chargeable_amount:
+                self.discount_type = 'fixed'
+                discount = amount_total - self.chargeable_amount
+                self.discount_percentage = (discount / amount_total) * 100
+            else:
+                self.discount_percentage = 0
+                self.discount = 0
         else:
-            self.discount_percentage = 0
-            self.discount = 0
-    else:
-        if self.discount:
-            self.discount_percentage = (self.discount / amount_total) * 100
-        else:
-            self.discount_percentage = 0
+            if self.discount:
+                self.discount_percentage = (self.discount / amount_total) * 100
+            else:
+                self.discount_percentage = 0
 
-        self.discount = amount_total * self.discount_percentage / 100 if self.discount_percentage else 0
+            self.discount = amount_total * self.discount_percentage / 100 if self.discount_percentage else 0
 
 
     @api.model
